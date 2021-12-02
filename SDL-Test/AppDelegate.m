b4e13b89f294dbdf1fad5f7ed8b43fac7533ff9e
Stefan Szaniszlo <stefan.szaniszlo@agnos.ai>
2021-12-02 23:32:18 +0000

    Initial Commit

---
+++ SDL-Test/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  SDL-Test
+//
+//  Created by Stefan Szaniszlo on 02/12/2021.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ SDL-Test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  SDL-Test
+//
+//  Created by Stefan Szaniszlo on 02/12/2021.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
